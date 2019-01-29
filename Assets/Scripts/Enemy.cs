55e9286915dbb8a4b874c536734f7374b0d63257
joshruane98 <joshuaruane@gmail.com>
2019-01-29 14:05:21 +0000

    Enemy Script Added
    Enemy Script containing all enemy attributes and all appropriate getters added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private int health;
+    [SerializeField] private int attack;
+    [SerializeField] private int defence;
+    [SerializeField] private int reflex;
+    [SerializeField] private int bravery;
+    [SerializeField] private int intelligence; //Used to determine chance of selecting the most sensible option in battle.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,36 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //RETRIVE STATS
+    int getHealth()
+    {
+        return health;
+    }
+
+    int getAttack()
+    {
+        return attack;
+    }
+
+    int getDefence()
+    {
+        return defence;
+    }
+
+    int getReflex()
+    {
+        return reflex;
+    }
+
+    int getBravery()
+    {
+        return bravery;
+    }
+
+    int getIntelligence()
+    {
+        return intelligence;
     }
 }
