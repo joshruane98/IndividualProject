310bff217c904df5799d6fda8e074f07cb63a943
joshruane98 <joshuaruane@gmail.com>
2019-03-18 18:13:15 +0000

    Made GameManager instance persistent accross all scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    static GameManager gameManagerInst;
+    
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
+        setupGM();
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             SceneManager.LoadScene(1);
         }
+    }
+
+    void setupGM()
+    {
+        //Check if there is already an Instance of Game Manager
+        if (gameManagerInst != null)
+        {
+            Destroy(this.gameObject);
+        }
+        gameManagerInst = this;
+        //Ensure Game Manager persists through all scenes
+        GameObject.DontDestroyOnLoad(this.gameObject);
     }
 }
