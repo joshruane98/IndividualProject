8ce63997e2490be8d1d73a954560aca67e9a8299
joshruane98 <joshuaruane@gmail.com>
2019-03-23 15:46:10 +0000

    Added basic NPC dialogue functionality
    Dialogue displayed when NPC interacted with. Dialogue box disappears after last sentence displayed.
    
    Next step: how to handle references between NPC's and UI. Continue button can only have reference to one NPC script. Separate Dialogue box and button attached to every NPC? Way to use GameManager to assign reference to appropriate NPC (maybe too complex).

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : Interactable
+{
+    [SerializeField] string npcName;
+    [SerializeField] string[] dialogue;
+    int i;
+    public Text dialogueDisplay;
+    public Button nextSentenceButton;
+    public override void interactAction()
+    {
+        dialogueDisplay.gameObject.SetActive(true);
+        nextSentenceButton.gameObject.SetActive(true);
+        Debug.Log("NPC says: " + dialogue[0]);
+        dialogueDisplay.text = dialogue[0];
+    }
+
+    public void displayNextSentence()
+    {
+        i++;
+        if (i <= dialogue.Length - 1)
+        {
+            dialogueDisplay.text = dialogue[i];
+        }
+        else
+        {
+            dialogueDisplay.gameObject.SetActive(false);
+            nextSentenceButton.gameObject.SetActive(false);
+        }
+    }
+}
