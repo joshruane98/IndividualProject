3d85e9c2c506be79e017c7dfd31ea58c2e0dfd49
joshruane98 <joshuaruane@gmail.com>
2019-04-22 11:36:54 +0100

    Added Main Menu
    Added main menu featuring game logo. The menu will be displayed upon starting the game and features buttons to start the game, quit the game and view instructions on how to play the game.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+    public void startGame()
+    {
+        SceneManager.LoadScene("Preload");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void displayInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void closeInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+}
